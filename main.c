a86bc5407d44129871938d4576415fb707d71f42
carlos <carlos.lima@udla.edu.ec>
2026-05-12 11:20:12 -0500

    segunda opcion de impresion de arreglo con punteros

--- main.c
+++ main.c
@@ -5,12 +5,12 @@ int main()
     int  *ptr;
 
     ptr=&v[0];
-   
+   // impresion de arreglo con punteros
     for (int i=0;i<5;i++)
     {
         printf("arreglo[%d]= %d ",i,v[i]);
-        ptr=&v[i];
-        printf("%d\n" ,*ptr);
+       // ptr=&v[i];
+        printf("%d\n" ,*(ptr+i));
     }
 
     return 0;
