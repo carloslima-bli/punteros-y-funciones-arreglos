ed130166760f7251b3bc4b8087fcd3ed27a219b2
carlos <carlos.lima@udla.edu.ec>
2026-05-12 11:14:10 -0500

    impresion de arreglo con punteros

--- main.c
+++ main.c
@@ -2,9 +2,15 @@
 int main()
 {
     int v[5]={2,4,6,8,10};
+    int  *ptr;
+
+    ptr=&v[0];
+   
     for (int i=0;i<5;i++)
     {
-        printf(" %d",v[i]);
+        printf("arreglo[%d]= %d ",i,v[i]);
+        ptr=&v[i];
+        printf("%d\n" ,*ptr);
     }
 
     return 0;
